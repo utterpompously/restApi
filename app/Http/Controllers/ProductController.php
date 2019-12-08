fac3be0ce707fae29b641bdf61561fca41d236d3
Utter Pompously <utterpompously@gmail.com>
2019-12-08 09:13:30 +0600

    Modified ProductController,passport auth,create auth,create product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
@@ -9,6 +10,9 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function _construct(){
+        $this->middleware('auth.api')->except('index','show');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -35,9 +39,21 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
+        $product = new Product;
 
+        $product->name = $request->name;
+        $product->detail = $request->detail;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->discount = $request->discount;
+
+        $product->save();
+
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
